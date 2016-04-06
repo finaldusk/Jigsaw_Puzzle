07c98b9ce7cbbd12b91206e67a1df73e10dfe372
finaldusk <finaldusk@live.cn>
2016-04-06 18:28:25 +0800

    参照Visual C#.NET基础与应用教程 编写的拼图软件，对书中例题拼图游戏进行了完善与BUG修正

---
+++ pintu/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace pintu
+{
+    partial class Main_Game
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_LoadIMG = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_Start);
+            this.groupBox1.Controls.Add(this.radioButton5);
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(100, 175);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "难度级别";
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Location = new System.Drawing.Point(12, 139);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(75, 23);
+            this.btn_Start.TabIndex = 5;
+            this.btn_Start.Text = "开始游戏";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(7, 113);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(77, 16);
+            this.radioButton5.TabIndex = 4;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "困难:7×7";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(7, 90);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(77, 16);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "中等:6×6";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(7, 67);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(77, 16);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "普通:5×5";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(7, 44);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(77, 16);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "一般:4×4";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(7, 21);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(77, 16);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "简单:3×3";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 195);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btn_LoadIMG
+            // 
+            this.btn_LoadIMG.Location = new System.Drawing.Point(25, 302);
+            this.btn_LoadIMG.Name = "btn_LoadIMG";
+            this.btn_LoadIMG.Size = new System.Drawing.Size(75, 23);
+            this.btn_LoadIMG.TabIndex = 2;
+            this.btn_LoadIMG.Text = "加载图片";
+            this.btn_LoadIMG.UseVisualStyleBackColor = true;
+            this.btn_LoadIMG.Click += new System.EventHandler(this.btn_LoadIMG_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 420);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // Main_Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_LoadIMG);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Main_Game";
+            this.Text = "拼图";
+            this.Load += new System.EventHandler(this.Main_Game_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_LoadIMG;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ pintu/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Collections;
+
+namespace pintu
+{
+    public partial class Main_Game : Form
+    {
+
+        public PictureBox[] PicBlock = new PictureBox[49];
+        int GameSize;
+        int MAP_WIDTH = 400;
+        int FirstBlock, SecondBlock;
+        bool isSwap;
+        int[] Position = new int[49];
+        Bitmap Source;
+        string filename;
+
+
+        private void SaveBmp()
+        {
+            Graphics g;
+            g = Graphics.FromImage(Source);
+            g.DrawImage(Image.FromFile(filename), 0, 0, MAP_WIDTH, MAP_WIDTH);
+        }
+
+        private void init(int n)
+        {
+            Random rdm;
+            ArrayList a1 = new ArrayList();
+            int t = 0;
+            rdm = new Random();
+            while (a1.Count < n * n)
+            {
+                t = rdm.Next(0, n * n);
+                if ((!a1.Contains(t)))
+                {
+                    a1.Add(t);
+                }
+            }
+            for (t = 0; t <= a1.Count - 1; t++)
+            {
+                Position[t] = Convert.ToInt16(a1[t]);
+            }
+
+        }
+
+        private void swap(object sender,System.EventArgs e)
+        {
+            PictureBox bClick = (PictureBox)sender;
+            int i = 0;
+            Image temp;
+            if (!isSwap)
+            {
+                isSwap = true;
+                FirstBlock = Convert.ToInt16(bClick.Tag);
+            }
+            else
+            {
+                label1.Text = "";
+                SecondBlock = Convert.ToInt16(bClick.Tag);
+                temp = PicBlock[SecondBlock].Image;
+                PicBlock[SecondBlock].Image = PicBlock[FirstBlock].Image;
+                PicBlock[FirstBlock].Image = temp;
+
+                isSwap = false;
+
+                i = Position[SecondBlock];
+                Position[SecondBlock] = Position[FirstBlock];
+                Position[FirstBlock] = i;
+
+                foreach(int s in Position)
+                {
+                    label1.Text = label1.Text + Position[s].ToString();
+                }
+                if (CheckWin())
+                {
+                    MessageBox.Show("恭喜过关", "提示");
+                }
+          }
+        }
+
+        private bool CheckWin()
+        {
+            
+            for(int t = 0; t < GameSize* GameSize; t++)//修正书上例题bug
+            {
+                if (Position[t] != t)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private Bitmap create_image(int n)
+        {
+            int W = 0;
+            W = MAP_WIDTH / GameSize;
+            Bitmap bit = new Bitmap(W, W);
+            Graphics g = Graphics.FromImage(bit);
+            Rectangle a = new Rectangle(0, 0, W, W);
+            Rectangle b = new Rectangle((n % GameSize) * W, n / GameSize * W, W, W);
+            g.DrawImage(Source, a, b, GraphicsUnit.Pixel);
+            return bit;
+        }
+
+
+        public Main_Game()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_LoadIMG_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();//打开打开文件窗口
+            if (!string.IsNullOrEmpty(openFileDialog1.FileName))//非空则加载图片
+            {
+                filename = openFileDialog1.FileName;
+                pictureBox1.Image = Image.FromFile(filename);
+                SaveBmp();
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            GameSize = 3;
+            init(GameSize);
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            GameSize = 4;
+            init(GameSize);
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            GameSize = 5;
+            init(GameSize);
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            GameSize = 6;
+            init(GameSize);
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            GameSize = 7;
+            init(GameSize);
+        }
+
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            //卸载上次的图片块 
+            int i = 0;
+            int BWidth = 0;
+            bool flag = true;
+            while (this.Controls.Count > 0 & flag == true)
+            {
+                flag = false;
+                foreach (Control a in this.Controls)
+                {
+                    if (a.Name.Length > 8)
+                    {
+                        if (a.Name.Substring(0, 8) == "PicBlock")
+                        {
+                            this.Controls.Remove(a);
+                            flag = true;
+                            i = i + 1;
+                        }
+                    }
+                }
+            }
+            init(GameSize);     //重新加载图片块 
+            BWidth = MAP_WIDTH / GameSize;
+            for (i = 0; i <= GameSize * GameSize - 1; i++)
+            {
+                PicBlock[i] = new PictureBox();
+                this.Controls.Add(PicBlock[i]);
+                PicBlock[i].Left = 160 + BWidth * (i % GameSize);//生成拼图左侧与窗体左侧的距离
+                PicBlock[i].Top = BWidth * (int)(i / GameSize) + 20;//生成拼图上侧与窗体上侧的距离
+                PicBlock[i].Width = BWidth;
+                PicBlock[i].Height = BWidth;
+                PicBlock[i].Name = "PicBlock" + i.ToString();
+                PicBlock[i].Tag = i;
+                PicBlock[i].Image = create_image(Position[i]);
+                PicBlock[i].BorderStyle = BorderStyle.FixedSingle;
+                //PicBlock[i].BringToFront() 
+                ((PictureBox)PicBlock[i]).Click += swap;
+            }
+        }
+
+        private void Main_Game_Load(object sender, EventArgs e)
+        {
+            filename = Application.StartupPath + "\\temp.bmp";
+            pictureBox1.Image = Image.FromFile(filename);
+            isSwap = false;
+            Source = new Bitmap(MAP_WIDTH, MAP_WIDTH);
+            SaveBmp();
+        }
+    }
+}
